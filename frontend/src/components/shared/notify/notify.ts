4bedec8862bbf12b1ae74d006961a509dcb24a86
fgmarcal <felipegarciamarcal@gmail.com>
2025-03-24 19:02:05 -0300

    fix: remove enum

--- frontend/src/components/shared/notify/notify.ts
+++ frontend/src/components/shared/notify/notify.ts
@@ -1,6 +1,5 @@
 import { message } from "antd";
 import axios from "axios";
-import { Errors } from "../../../assets/enums/errors";
 
 export const notifySuccess = (msg:string) => {
     message.success(msg);
@@ -9,10 +8,9 @@ export const notifySuccess = (msg:string) => {
 export const notifyError = (error:unknown) =>{
     if (axios.isAxiosError(error)) {
         const errorMsg = error.response?.data?.message;
-        const errorType = error.response?.data?.error as keyof typeof Errors;
 
         console.error(errorMsg);
-        message.error(Errors[errorType]);
+        message.error(errorMsg);
     } else {
         message.error('Erro inesperado');
     }
